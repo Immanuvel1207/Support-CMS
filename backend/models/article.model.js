e53fff8f5379b66a4e352329e353f160c7c9ac35
Dhivyakanagaraj <dhivyasubashinik.22aid@kongu.edu>
2025-05-02 10:18:33 +0530

    other models added

---
+++ backend/models/article.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  tags: [String],
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'ContentCreator',
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Article', articleSchema);
